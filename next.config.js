993fa855eb302d9611dfab185df17d7bb695936f
satishsahu25 <satishk7619@gmail.com>
2023-12-05 11:04:07 +0530

    fsddc

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [{
-                    key:"Access-Control-Allow-Origin",value:"*",
-                },{
-                    key:'Access-Control-Allow-Methods',value:['GET', 'POST', 'PUT', 'DELETE']
-                }]
-                    
-            }
-        ]
-    }
-
 }
 
 module.exports = nextConfig
